fc0b91efb6f831d8d7a31f1647294c7eaf067efd
ldthorson90 <ldthorson90@gmail.com>
2016-10-25 20:41:50 -0400

    Update blink.js

--- chapter4-gpios/blink.js
+++ chapter4-gpios/blink.js
@@ -1,28 +1,17 @@
-var piGpio = require('gpio'); //#A
+var wpi = require('../lib/exports');
 
-var Gpio = gpio.Gpio,
-  led = new Gpio(9, 'out'), //#B
-  interval;
+wpi.setup();
 
-interval = setInterval(function () { //#C
-  var value = (led.readSync() + 1) % 2; //#D
-  led.write(value, function() { //#E
-    console.log("Changed LED state to: " + value);
-  });
-}, 2000);
+var pin = 9;
 
-process.on('SIGINT', function () { //#F
-  clearInterval(interval);
-  led.writeSync(0); //#G
-  led.unexport();
-  console.log('Bye, bye!');
-  process.exit();
-});
+wpi.pinMode(pin, wpi.modes.OUTPUT);
 
-// #A Import the pi-gpio library
-// #B Initialize pin 9 to be an output pin
-// #C This interval will be called every 2 seconds
-// #D Synchronously read the value of pin 9 and transform 1 to 0 or 0 to 1
-// #E Asynchronously write the new value to pin 9
-// #F Listen to the event triggered on CTRL+C
-// #G Cleanly close the GPIO pin before exiting
+var value = 1;
+
+setInterval(function() {
+  wpi.digitalWrite(pin, value);
+  value = +!value;
+}, 500);
+
+process.on('SIGINT', function () {
+    clearInterval(interval)
